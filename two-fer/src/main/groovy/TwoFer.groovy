373b207de246475947b8c5cf7a5ff589aa02047c
photonn <photonn@hotmail.com>
2019-10-24 16:29:55 +0200

    Solution to TwoFer

---
+++ two-fer/src/main/groovy/TwoFer.groovy
@@ -0,0 +1,10 @@
+class TwoFer {
+
+    static String twoFer(String name) {
+        if (!name){
+            return "One for you, one for me.";
+        }else{
+            return ("One for " + name + ", one for me.");
+        }
+    }
+}
